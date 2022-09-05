e4d5e31d4d25787c0954e18c869b2873c57d1000
진고은 <1201silver@naver.com>
2022-09-05 14:26:02 +0900

    SearchActivity 추가

--- app/src/main/java/com/hanium/android/mydata/MainActivity.java
+++ app/src/main/java/com/hanium/android/mydata/MainActivity.java
@@ -2,12 +2,23 @@ package com.hanium.android.mydata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v){
+        switch (v.getId()) {
+            case R.id.searchBtn:
+                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
