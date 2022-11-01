1b0a16030a8d7824627d5a50ed14bd1ee9af0b10
JURI <jr4426@naver.com>
2022-11-01 23:35:55 +0900

    main2 push

--- app/src/main/java/com/hanium/android/mydata/MainActivity2.java
+++ app/src/main/java/com/hanium/android/mydata/MainActivity2.java
@@ -58,6 +58,7 @@ public class MainActivity2 extends AppCompatActivity {
         });
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
