2ab12b72d263e2f9c2793edd2e55e4467c3c97ad
이지영 <wldud2550@naver.com>
2022-11-08 01:32:33 +0900

    리스트뷰 클릭 임시

--- app/src/main/java/com/hanium/android/mydata/ui/benefit/BenefitActivity.java
+++ app/src/main/java/com/hanium/android/mydata/ui/benefit/BenefitActivity.java
@@ -57,28 +57,41 @@ public class BenefitActivity extends AppCompatActivity {
 //        adapter = new BenefitAdapter(this, R.layout.adapter_benefit, null);
 //        lvContacts.setAdapter(adapter);
 
-
 //        		리스트 뷰 클릭 처리
-//        lvContacts.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            Intent intent;
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-//                SQLiteDatabase db = helper.getReadableDatabase();
-//                cursor = db.rawQuery("select * from contact_table where _id=" + id, null);
-//                ContactDto dto = new ContactDto();
-//                while(cursor.moveToNext()){
-//                    dto.setId(cursor.getLong(cursor.getColumnIndex(ContactDBHelper.COL_ID)));
-//                    dto.setSong(cursor.getString(cursor.getColumnIndex(ContactDBHelper.COL_SONG)));
-//                    dto.setSinger(cursor.getString(cursor.getColumnIndex(ContactDBHelper.COL_SINGER)));
-//                    dto.setGenre(cursor.getString(cursor.getColumnIndex(ContactDBHelper.COL_GENRE)));
-//                    dto.setDate(cursor.getString(cursor.getColumnIndex(ContactDBHelper.COL_DATE)));
-//                }
-//                intent = new Intent(AllContactsActivity.this, UpdateActivity.class);
-//                intent.putExtra("dto", dto);
-//                startActivityForResult(intent, UPDATE_ACTIVITY_CODE);
-//            }
-//        });
-/*
+        lvContacts.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Go to Detail Activity
+                Intent i = new Intent(BenefitActivity.this, BenefiltDetailActivity.class);
+                // Send the position number to Detail Activity too.
+                i.putExtra("position", position);
+                // Run the process
+                startActivity(i);
+            }
+        });
+        /*
+//        		리스트 뷰 클릭 처리
+
+        lvContacts.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            Intent intent;
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                SQLiteDatabase db = helper.getReadableDatabase();
+                cursor = db.rawQuery("select * from contact_table where _id=" + id, null);
+                ContactDto dto = new ContactDto();
+                while(cursor.moveToNext()){
+                    dto.setId(cursor.getLong(cursor.getColumnIndex(ContactDBHelper.COL_ID)));
+                    dto.setSong(cursor.getString(cursor.getColumnIndex(ContactDBHelper.COL_SONG)));
+                    dto.setSinger(cursor.getString(cursor.getColumnIndex(ContactDBHelper.COL_SINGER)));
+                    dto.setGenre(cursor.getString(cursor.getColumnIndex(ContactDBHelper.COL_GENRE)));
+                    dto.setDate(cursor.getString(cursor.getColumnIndex(ContactDBHelper.COL_DATE)));
+                }
+                intent = new Intent(AllContactsActivity.this, UpdateActivity.class);
+                intent.putExtra("dto", dto);
+                startActivityForResult(intent, UPDATE_ACTIVITY_CODE);
+            }
+        });
+
 //		리스트 뷰 롱클릭 처리
         lvContacts.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
