b9df588ab44e5f7a4d96442a53af427cff6cef34
진고은 <1201silver@naver.com>
2022-11-20 18:58:38 +0900

    뒤로가기 수정

--- app/src/main/java/com/hanium/android/mydata/ui/benefit/BenefiltDetailActivity.java
+++ app/src/main/java/com/hanium/android/mydata/ui/benefit/BenefiltDetailActivity.java
@@ -3,6 +3,7 @@ package com.hanium.android.mydata.ui.benefit;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.hanium.android.mydata.R;
 
@@ -11,6 +12,11 @@ public class BenefiltDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_benefit_detail);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.benefit_detail_toolbar);
+        setSupportActionBar(mToolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // 뒤로가기
     }
 
 }
