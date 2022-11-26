ab2daf4d133c2d8af3e0f0bbf16f9211f55ddf7d
Hyeyeon Lee <tjsalsu@naver.com>
2022-11-27 03:17:50 +0900

    회원탈퇴,로딩xml,앱로고

---
+++ app/src/main/java/com/hanium/android/mydata/ui/LoadingActivity.java
@@ -0,0 +1,27 @@
+package com.hanium.android.mydata.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.hanium.android.mydata.R;
+
+public class LoadingActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        startLoading();
+    }
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 2000);
+    }
+}
---
+++ app/src/main/java/com/hanium/android/mydata/ui/mypage/WithdrawRequest.java
@@ -0,0 +1,31 @@
+package com.hanium.android.mydata.ui.mypage;
+
+import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WithdrawRequest extends StringRequest {
+    final static String TAG = "WithdrawActivity";
+
+    final static private String URL = "http://192.168.133.1/withdraw_user.php";
+    private Map<String, String> map;
+
+    public WithdrawRequest(String userID, Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null);
+
+        Log.d(TAG, "in LoginRequest: " +userID+ " " +userID);
+
+        map = new HashMap<>();
+        map.put("userID", userID);
+
+    }
+
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
+}
