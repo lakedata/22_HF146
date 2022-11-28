db014c17608c1789424dee46552a9151cce0d1b3
진고은 <1201silver@naver.com>
2022-11-28 23:34:33 +0900

    자주 찾는 앱 (외부 앱 연결 O)

--- app/src/main/java/com/hanium/android/mydata/ui/favApp/FavAppActivity.java
+++ app/src/main/java/com/hanium/android/mydata/ui/favApp/FavAppActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
@@ -19,6 +20,8 @@ public class FavAppActivity extends AppCompatActivity {
 
     final static String TAG = "FavAppActivity";
 
+    private String packageName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,41 +32,73 @@ public class FavAppActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+
     }
 
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.app_T: //cj는 열리는데 t멤버십은 안됨 .. 엥
+                packageName = "com.tms";
+                openApp(packageName);
+                break;
+            case R.id.app_LG:
+                packageName = "uplus.membership";
+                openApp(packageName);
+                break;
+            case R.id.app_KT:
+                packageName = "com.olleh.android.oc";
+                openApp(packageName);
+                break;
+            case R.id.app_CJ:
+                packageName = "kr.co.ivlog.mobile.app.cjonecard";
+                openApp(packageName);
+                break;
+            case R.id.app_SB:
+                packageName = "com.starbucks.co";
+                openApp(packageName);
+                break;
+            case R.id.app_GC:
+                packageName = "com.easymembership.gongcha";
+                openApp(packageName);
+                break;
             case R.id.chromeBtn:
-//                String packageName = "com.android.chrome";
-                String packageName = "com.instagram.android";
-
-                if (checkAppExist(packageName)) {
-                    Log.d(TAG, String.valueOf(checkAppExist(packageName)));
-                    Intent intent = this.getPackageManager().getLaunchIntentForPackage(packageName);
-//                Intent intent = this.getLaunchIntentForPackage(packageName);
-                    startActivity(intent);
-                }
-                else {
-                    Log.d(TAG, "checkAppExist: " +String.valueOf(checkAppExist(packageName))+ " -> open playstore");
-
-                    try {
-                        Intent intent = new Intent(Intent.ACTION_VIEW);
-                        intent.setData(Uri.parse("market://details?id=" +packageName));
-                        startActivity(intent);
-                        Log.d(TAG, "open app");
-                    } catch (Exception e) {
-                        Intent intent = new Intent(Intent.ACTION_VIEW);
-                        intent.setData(Uri.parse("https://play.google.com/store/apps/details?id=" +packageName));
-                        startActivity(intent);
-                        Log.d(TAG, "open browser");
-                    }
-                }
-
+                packageName = "com.google.android.youtube";
+                openApp(packageName);
                 break;
         }
     }
 
+    public void openApp(String packageName) {
+
+        if (checkAppExist(packageName)) {
+            Log.d(TAG, String.valueOf(checkAppExist(packageName)));
+            Intent intent = this.getPackageManager().getLaunchIntentForPackage(packageName);
+//                Intent intent = this.getLaunchIntentForPackage(packageName);
+            startActivity(intent);
+        }
+        else {
+            Log.d(TAG, "checkAppExist: " +String.valueOf(checkAppExist(packageName))+ " -> open playstore");
+
+            try {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("market://details?id=" +packageName));
+                startActivity(intent);
+                Log.d(TAG, "open app");
+
+            } catch (Exception e) {
+                Log.d(TAG, e.getMessage());
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://play.google.com/store/apps/details?id=" +packageName));
+                startActivity(intent);
+                Log.d(TAG, "open browser");
+            }
+        }
+
+    }
+
     public boolean checkAppExist(String packageName) {
+
+        Log.d(TAG, "in checkAppExist -> " +packageName);
 
         boolean exist = false;
 
@@ -72,14 +107,18 @@ public class FavAppActivity extends AppCompatActivity {
         }
 
         PackageManager pm = getPackageManager();
-        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
-        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-
-        List<ResolveInfo> apps = pm.queryIntentActivities(mainIntent, 0);
+//        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+//        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+//
+//        List<ResolveInfo> apps = pm.queryIntentActivities(mainIntent, 0);
+        List<PackageInfo> apps = pm.getInstalledPackages(0);
+        Log.d(TAG, "apps size: " +apps.size());
 
         try {
             for (int i = 0; i < apps.size(); i++){
-                if (apps.get(i).activityInfo.packageName.startsWith(packageName)) {
+
+//                if (apps.get(i).activityInfo.packageName.startsWith(packageName)) {
+                if(apps.get(i).packageName.startsWith(packageName)) {
                     exist = true;
                     break;
                 }
